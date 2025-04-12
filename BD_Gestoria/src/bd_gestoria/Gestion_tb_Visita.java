61dffb17ff5a27921c77e0178307dda4f45fca0e
samue <samue@PC_Samuel>
2025-04-12 20:01:20 +0200

    Clase Gestion_tb_visita: quetar buscar usuario

--- BD_Gestoria/src/bd_gestoria/Gestion_tb_Visita.java
+++ BD_Gestoria/src/bd_gestoria/Gestion_tb_Visita.java
@@ -7,25 +7,15 @@ import java.sql.SQLException;
 
 public class Gestion_tb_Visita {
 
-    public static boolean ComprobarUsuario(String codigo) throws SQLException {
-        try (Connection conexion = ConexionBaseDatos.getConnection()) {
-            if (conexion != null) {
-                try {
-                    PreparedStatement stmt = conexion.prepareStatement("SELECT * FROM clientes WHERE dni=?");
-                    stmt.setString(1, codigo);
-                    ResultSet resultado = stmt.executeQuery();
-                    while (resultado.next()) {
-                        String dni = resultado.getString(1);
-                        if (dni.equals(codigo)) {
-                            return true;
-                        }
-                    }
-                } catch (SQLException e) {
-                    System.out.println("Error. Debe introducir un numero.\n");
-                }
+    public static void agregarVisita() {
+        Connection conexion = ConexionBaseDatos.getConnection();
+        if (conexion != null) {
+            try {
+                var stmt=conexion.prepareStatement("INSERT INTO tb_visitas (NIF, Fecha_visita, Consulta, ValorConsulta) VALUES (?,?,?,?)");
+                
+            } catch (SQLException e) {
+                System.out.println("Error al añadir una sentencia:" + e.getMessage());
             }
         }
-        return false;
     }
-
 }
