711237fb550367fc45d944772f21260578a74e8a
samue <samue@PC_Samuel>
2025-04-12 18:42:19 +0200

    Clase main: añadir un cin

--- BD_Gestoria/src/bd_gestoria/BD_Gestoria.java
+++ BD_Gestoria/src/bd_gestoria/BD_Gestoria.java
@@ -1,8 +1,11 @@
 package bd_gestoria;
 
+import java.util.Scanner;
+
 public class BD_Gestoria {
 
-    public static void main(String[] args){
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
     }
 }
