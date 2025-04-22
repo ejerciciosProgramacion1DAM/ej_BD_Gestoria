6f4758f9eff728a7d8b09c40a9fda08581f37cf1
smunpov1010 <smunpov1010@PC111918.dptoinformatica.iesfuengirola1.net>
2025-04-22 18:32:34 +0200

    commit

--- BD_Gestoria/src/bd_gestoria/Gestion_tb_Visita.java
+++ BD_Gestoria/src/bd_gestoria/Gestion_tb_Visita.java
@@ -1,31 +1,38 @@
 package bd_gestoria;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Gestion_tb_Visita {
 
-    public static void agregarVisita(Scanner in) {
-        Connection conexion = ConexionBaseDatos.getConnection();
-        if (conexion != null) {
-            try {
-                var stmt=conexion.prepareStatement("INSERT INTO tb_visitas (NIF, Fecha_visita, Consulta, ValorConsulta) VALUES (?,?,?,?)");
-                stmt.setString(1, Visita.setNIF(in));
-                stmt.setString(2, Visita.setFecha_visita(in));
-                String visita=Visita.setVisita(in);
+    public static void agregarVisita(Connection conexion, Scanner in) {
+        try {
+            String nif = Visita.getNIF(in);
+            if (conexion != null && Gestion_tb_Usuarios.buscarUsuario(nif)) {
+                var stmt = conexion.prepareStatement("INSERT INTO tb_visitas (NIF, Fecha_visita, Consulta, ValorConsulta) VALUES (?,?,?,?)");
+                stmt.setString(1, nif);
+                stmt.setString(2, Visita.getFecha_visita(in));
+                String visita = Visita.getVisita(in);
                 stmt.setString(3, visita);
-                stmt.setInt(4, Visita.setVisitaValor(visita));
+                stmt.setInt(4, Visita.getVisitaValor(visita));
                 stmt.execute();
-            } catch (SQLException e) {
-                System.out.println("Error al añadir una sentencia:" + e.getMessage());
             }
+        } catch (SQLException e) {
+            System.out.println("Error al añadir una sentencia:" + e.getMessage());
         }
     }
 
-
-
+    public static void eliminarVisita(Connection conexion, Scanner in) {
+        if (conexion != null) {
+            try {
+                var stmt = conexion.prepareStatement("DELETE FROM tb_visitas WHERE NIF=?");
+                stmt.setString(1, Visita.getNIF(in));
+                stmt.execute();
+            } catch (SQLException e) {
+                System.out.println("ha ocurrido un error al eliminar a una visita: " + e.getMessage());
+            }
+        }
+    }
 
 }
